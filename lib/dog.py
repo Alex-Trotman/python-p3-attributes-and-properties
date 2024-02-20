a7bba83576d202ff53f83f48cbb5bcc977458645
Alex <alextrotman04@gmail.com>
2024-02-19 21:28:55 -0600

    save

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,19 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name=""):
+        # Directly use the name property to trigger validation.
+        self.name = name
+
+    def get_name(self):
+        # Getter simply returns the value of _name.
+        return self._name
+
+    def set_name(self, value):
+        # Setter includes validation for the name.
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            # Print the specified message if validation fails.
+            print("Name must be string between 1 and 25 characters.")
+    name = property(get_name, set_name)
